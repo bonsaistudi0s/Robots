a2a3ee5be9bfa30d01ad7ecadb3d85a21b896292
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-01 01:41:20 +0200

    👻 Oops

--- common/src/main/java/dev/xylonity/bonsai/robots/client/entity/layer/GenericMechElectricFieldLayer.java
+++ common/src/main/java/dev/xylonity/bonsai/robots/client/entity/layer/GenericMechElectricFieldLayer.java
@@ -1,12 +1,13 @@
 package dev.xylonity.bonsai.robots.client.entity.layer;
 
 import dev.xylonity.bonsai.robots.common.entity.AbstractMechEntity;
+import dev.xylonity.knightlib.client.animation.layer.impl.KnightLibEmissiveLayer;
 import dev.xylonity.knightlib.client.animation.layer.impl.KnightLibOverlayLayer;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.Function;
 
-public class GenericMechElectricFieldLayer<T extends AbstractMechEntity> extends KnightLibOverlayLayer<T> {
+public class GenericMechElectricFieldLayer<T extends AbstractMechEntity> extends KnightLibEmissiveLayer<T> {
 
     public GenericMechElectricFieldLayer(Function<T, ResourceLocation> texture) {
         super(texture);
