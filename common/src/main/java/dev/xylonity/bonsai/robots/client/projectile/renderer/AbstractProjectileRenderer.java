3923522ff1491a12ad915f063bdd9929140b573a
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-11 23:25:37 +0200

    💛 Lasers are now rotated to the movement direction

--- common/src/main/java/dev/xylonity/bonsai/robots/client/projectile/renderer/AbstractProjectileRenderer.java
+++ common/src/main/java/dev/xylonity/bonsai/robots/client/projectile/renderer/AbstractProjectileRenderer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -41,8 +42,23 @@ public abstract class AbstractProjectileRenderer<T extends Entity & KnightLibAni
 
     @Override
     protected void setupPose(T projectile, KnightLibModel model, float partialTicks) {
-        final float pitch = Mth.lerp(partialTicks, projectile.xRotO, projectile.getXRot());
-        model.applyRotation("main", -pitch, 0.0F, 0.0F);
+        final float renderedYaw = Mth.rotLerp(partialTicks, projectile.yRotO, projectile.getYRot());
+        final Vec3 motion = projectile.getDeltaMovement();
+
+        final float trajectoryYaw;
+        final float trajectoryPitch;
+        if (motion.lengthSqr() > 1.0E-8D) {
+            final double horizontalSpeed = Math.sqrt(motion.x * motion.x + motion.z * motion.z);
+            trajectoryYaw = (float) Math.toDegrees(Math.atan2(motion.x, motion.z));
+            trajectoryPitch = (float) Math.toDegrees(Math.atan2(motion.y, horizontalSpeed));
+        }
+        else {
+            trajectoryYaw = renderedYaw;
+            trajectoryPitch = Mth.lerp(partialTicks, projectile.xRotO, projectile.getXRot());
+        }
+
+        final float yawCorrection = Mth.wrapDegrees(trajectoryYaw + renderedYaw);
+        model.applyRotation("main", -trajectoryPitch, -yawCorrection, 0.0F);
     }
 
 }
---
+++ common/src/main/java/dev/xylonity/bonsai/robots/client/projectile/renderer/GenericLaserProjectileRenderer.java
@@ -0,0 +1,44 @@
+package dev.xylonity.bonsai.robots.client.projectile.renderer;
+
+import dev.xylonity.bonsai.robots.Robots;
+import dev.xylonity.bonsai.robots.client.util.RobotsRenderTypes;
+import dev.xylonity.bonsai.robots.common.entity.projectile.LaserProjectileEntity;
+import dev.xylonity.knightlib.client.animation.model.KnightLibModel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public class GenericLaserProjectileRenderer<T extends LaserProjectileEntity> extends AbstractProjectileRenderer<T> {
+
+    private final ResourceLocation electricTexture;
+    private final float scale;
+
+    public GenericLaserProjectileRenderer(EntityRendererProvider.Context context, String name, float scale) {
+        super(context, name, null);
+        this.electricTexture = Robots.of("textures/entity/projectile/" + name + "_electric.png");
+        this.scale = scale;
+    }
+
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull T laser) {
+        return laser.isElectric() ? this.electricTexture : super.getTextureLocation(laser);
+    }
+
+    @Override
+    protected RenderType getRenderType(T laser, ResourceLocation texture) {
+        return RobotsRenderTypes.entityEmissiveDepth(texture);
+    }
+
+    @Override
+    protected float getScale(T laser) {
+        return this.scale;
+    }
+
+    @Override
+    protected void setupPose(T laser, KnightLibModel model, float partialTicks) {
+        super.setupPose(laser, model, partialTicks);
+        model.applyScale("main", 1.0F, 1.0F, 2.0F);
+    }
+
+}
